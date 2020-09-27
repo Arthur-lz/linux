97bf6a363f9553b3ac7d325fbb7004bf74a3f7a9
Arthur-lz <twuufnxlz@163.com>
2020-09-28 06:58:07 +0800

    x86_64 qemu done

---
+++ init_x86_64.sh
@@ -0,0 +1,61 @@
+#!/bin/fish
+#初始化busybox生成的文件系统
+#
+# 
+sudo rm -rf rootfs
+sudo rm -rf tmpfs
+sudo rm -rf ramdisk*
+sudo mkdir rootfs
+sudo cp ../busybox-1.27.1/_install_x86_64/*  rootfs/ -raf
+#sudo mkdir -p rootfs/proc/
+#sudo mkdir -p rootfs/sys/
+#sudo mkdir -p rootfs/tmp/
+sudo mkdir -p rootfs/root/
+sudo mkdir -p rootfs/var/
+#sudo mkdir -p rootfs/mnt/
+# etc 
+sudo mkdir -p rootfs/etc/init.d/
+sudo touch rootfs/etc/init.d/rcS
+sudo echo "mkdir -p proc"\n\
+"mkdir -p /tmp"\n\
+"mkdir -p /sys"\n\
+"mkdir -p /mnt"\n\
+"/bin/mount -a"\n\
+"mkdir -p /dev/pts"\n\
+"mount -t devpts devpts /dev/pts"\n\
+"echo /sbin/mdev > /proc/sys/kernel/hotplug"\n\
+"mdev -s" > rootfs/etc/init.d/rcS
+sudo chmod +x rootfs/etc/init.d/rcS
+
+sudo touch rootfs/etc/fstab
+sudo echo "proc /proc proc defaults 0 0"\n\
+"tmpfs /tmp tmpfs defaults 0 0"\n\
+"sysfs /sys sysfs defaults 0 0"\n\
+"tmpfs /dev tmpfs defaults 0 0"\n\
+"debugfs /sys/kernel/debug debugfs defaults 0 0" > rootfs/etc/fstab
+sudo touch rootfs/etc/inittab
+sudo echo "::sysinit:/etc/init.d/rcS"\n\
+"::respawn:-/bin/sh"\n\
+"::askfirst:-/bin/sh"\n\
+"::ctrlaltdel:/bin/umount -a -r" > rootfs/etc/inittab
+
+sudo mkdir -p rootfs/lib64
+sudo cp /usr/lib64/ld-linux-x86-64.so.2 rootfs/lib64/
+sudo rm rootfs/lib64/*.a
+sudo strip rootfs/lib64/*
+sudo mkdir -p rootfs/usr/lib/
+sudo cp /usr/lib/libc.so.6 rootfs/usr/lib/
+sudo mkdir -p rootfs/dev/
+sudo mknod rootfs/dev/tty1 c 4 1
+sudo mknod rootfs/dev/tty2 c 4 2
+sudo mknod rootfs/dev/tty3 c 4 3
+sudo mknod rootfs/dev/tty4 c 4 4
+sudo mknod rootfs/dev/console c 5 1
+sudo mknod rootfs/dev/null c 1 3
+sudo dd if=/dev/zero of=ramdisk bs=1M count=32
+sudo mkfs.ext4 -F ramdisk
+sudo mkdir -p tmpfs
+sudo mount -t ext4 ramdisk ./tmpfs/  -o loop
+sudo cp -raf rootfs/*  tmpfs/
+sudo umount tmpfs
+sudo gzip --best -c ramdisk > ramdisk.gz
--- next.md
+++ next.md
@@ -2,3 +2,9 @@
 0.准备gcc编译不优化版本的linux4.0.1内核镜像，用于qemu调试时使用
 1.Anjuta环境学习；替换调试工具，其默认使用系统自带的gdb，是否可换成gdb-mltiarch?或其他的如cgdb?
 2.准备x86_64、x86根文件系统
+3.将新环境连接开发板调通
+
+---
+* busybox 做x86_64根文件系统make时问题记录
+1. 以静态方式make时, 会出现找不到crypt,m等静态库问题
+2. 以动态方式make时，在qemu加载时会出现无法加载linuxrc即busybox问题
