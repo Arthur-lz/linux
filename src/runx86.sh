b4b1bd1f7f724ce60822f85c9ae1b3e84b9b35a8
Arthur-lz <twuufnxlz@163.com>
2020-09-28 18:59:38 +0800

    cc1 err code mode unsupply

--- src/runx86.sh
+++ src/runx86.sh
@@ -1,2 +1,2 @@
 #!/bin/fish
-qemu-system-x86_64 -m 2048M -kernel ./linux-4.19.24/arch/x86/boot/bzImage -append "root=/dev/ram0 rw rootfstype=ext4 console=ttyS0 init=/linuxrc" -nographic -initrd ./someone/ramdisk.gz
+qemu-system-x86_64 -m 2048M -kernel ./linux-4.19.24/arch/x86/boot/bzImage -append "root=/dev/ram0 rw rootfstype=ext4 console=ttyS0 init=/linuxrc" -nographic -initrd ./someone/ramdisk.gz -S -s
