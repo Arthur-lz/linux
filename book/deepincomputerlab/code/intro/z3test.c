2ecb0ca5a062ad6275119584394fa8aefb1255e1
Arthur-lz <twuufnxlz@163.com>
2021-05-13 18:01:49 +0800

    3.9.1结构

--- book/deepincomputerlab/code/intro/z3test.c
+++ book/deepincomputerlab/code/intro/z3test.c
@@ -37,9 +37,48 @@ void it_eq(int n, int A[n][n], int i)
 	else
 		printf("!=");
 }
+struct prob{
+	int *p;
+	struct {
+		int x;
+		int y;
+	} s;
+	struct prob *next;
+};
+
+void test_struct_and()
+{
+	struct prob sp;
+	struct prob *ssp = &sp;
+	if(&ssp->s.x == &(ssp->s.x))
+		printf("&s==&(s)");
+	else
+		printf("!=");
+	
+}
+
+double uu2double(unsigned w1, unsigned w2)
+{
+	union{
+		double d;
+		unsigned u[2];
+	}temp;
+	temp.u[0] = w1;
+	temp.u[1] = w2;
+	printf("u0 is: %d\n", temp.u[0]);
+	printf("u1 is: %d\n", temp.u[1]);
+	return temp.d;
+}
 
 void main()
 {
+	unsigned w1 = 17;//00010001 
+	unsigned w2 = 16;//00010000
+	double a1 = uu2double(17,16);
+	printf("a1 is: %.3f", a1);
+	return;
+	test_struct_and();
+	return;
 	int n = 5;
 	int A[n][n];
 	it_eq(5, A, 3);
